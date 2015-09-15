aed18e16b3095ff6c2d75ad9fd83439cdd9c7430
laepen <tobnie@student.chalmers.se>
2015-09-15 20:18:08 +0200

    djpwa

--- src/Train.java
+++ src/Train.java
@@ -57,46 +57,51 @@ public class Train implements Runnable {
 			new Semaphore(1)
 	};
 
-	protected SensorEvent[] events = new SensorEvent[] {
-			new SensorEvent(id,4,13,SensorEvent.ACTIVE),
-			new SensorEvent(id,4,11,SensorEvent.ACTIVE),
-			new SensorEvent(id,3,9,SensorEvent.ACTIVE)
-	};
+	//	protected SensorEvent[] events = new SensorEvent[] {
+	//			new SensorEvent(id,4,13,SensorEvent.ACTIVE),
+	//			new SensorEvent(id,4,11,SensorEvent.ACTIVE),
+	//			new SensorEvent(id,1,9,SensorEvent.ACTIVE)
+	//	};
 
 
 
 	public void checkEvent() throws CommandException, InterruptedException {
 
 		SensorEvent event = tsi.getSensor(id);
-		switch(this.direction){
+		if(event.getStatus() == SensorEvent.ACTIVE){
 
-			case (DOWNWARD):{}
+			switch(this.direction){
+
+				case (DOWNWARD):{}
 	
 	
-			case(UPWARD): {	
-				if(event.getXpos() == 4 && event.getYpos() == 11) {
+				case(UPWARD): {	
+					if(event.getXpos() == 4 && event.getYpos() == 11) {
 	
-					semArray[0].acquire();
-					tsi.setSwitch(3, 13, 2);		
-				}	
-				if(event.getXpos() == 3 && event.getYpos() == 9){
-					semArray[0].release();
-				}
-	
-				if(event.getXpos() == 4 && event.getYpos() == 13){
-					semArray[0].acquire();
-					tsi.setSwitch(3, 13, 1);		
-				}	
-				if(event.getXpos() == 1 && event.getYpos() == 10){
-					semArray[0].release();
-					if(!semArray[1].tryAcquire()){
-						tsi.setSwitch(4, 9, 2);
+						semArray[0].acquire();
+						tsi.setSwitch(3, 13, 2);		
+					}	
+					if(event.getXpos() == 3 && event.getYpos() == 9){
+						semArray[0].release();
 					}
-					else{
-						tsi.setSwitch(4, 9, 1);
-					}
-				}
 	
+					if(event.getXpos() == 4 && event.getYpos() == 13){
+						semArray[0].acquire();
+						tsi.setSwitch(3, 13, 1);		
+					}	
+					if(event.getXpos() == 1 && event.getYpos() == 10){
+						semArray[0].release();
+						if(!semArray[1].tryAcquire()){
+							tsi.setSwitch(4, 9, 2);
+							tsi.setSwitch(15, 9, 2);
+						}
+						else{
+							tsi.setSwitch(4, 9, 1);
+							tsi.setSwitch(15, 9, 1);
+						}
+					}
+	
+				}
 			}
 		}
 	}
